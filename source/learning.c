e7e6d6afc86fa74b2fa7f9258fd26a87407c0bf2
azaika <takaaza@gmail.com>
2020-12-29 05:27:20 +0900

    update learning.c

--- source/learning.c
+++ source/learning.c
@@ -1,6 +1,9 @@
 #include "ai.h"
 #include "ga.h"
 
+#include <stdio.h>
+#include <string.h>
+
 int main() {
     const int n = 200;
 
@@ -11,4 +14,23 @@ int main() {
         ai_generate_random_seed(&seeds[n], &rng);
     }
     ga_advance_turn(n, seeds, 100);
+
+    int top[1];
+    ga_do_tournament(200, seeds, 1, top);
+    
+    char buf[256];
+    ai_serialize_seed(&seeds[top[0]], buf);
+
+    size_t len = strlen(buf);
+
+    FILE* file = fopen("result", "wb");
+    if (file == NULL) {
+        fprintf(stderr, "failed to open 'result'.\n");
+        return 0;
+    }
+
+    if (fwrite(buf, sizeof(char), len, file) != len * sizeof(char)) {
+        fprintf(stderr, "cannot write result to file.\n");
+    }
+    fclose(file);
 }
