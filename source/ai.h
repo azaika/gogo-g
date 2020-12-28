c577ae9b53025dff5b565b2df6ddd4d366319c6e
azaika <takaaza@gmail.com>
2020-12-28 15:17:27 +0900

    implement ai_evaluate() partially

--- source/ai.h
+++ source/ai.h
@@ -33,25 +33,48 @@ static ai_seed* ai_deserialize_seed(const char* str) {
     // ToDo: implement
     return NULL;
 }
+ 
+static int count_pieces(game_state state, piece_type type, bool is_first, bool is_tegoma, bool promoted) {
+    int ret = 0;
+    for (int i = 0; i < 2; ++i) {
+        if (is_first != is_first_ones(state[type * 2 + i])) {
+            continue;
+        }
+        if (is_tegoma) {
+            if (get_coord(state[type * 2 + i]) == TEGOMA) {
+                ++ret;
+            }
+        }
+        else if (promoted != is_promoted(state[type * 2 + i])) {
+            ++ret;
+        }
+    }
 
-//評価関数
-int eval( /*引数教えていただきたいです*/  ){
+    return ret;
+}
+
+// 評価関数
+// is_first : 先手番のターンかどうか
+static int ai_evaluate(game_state state, bool is_first) {
     int value = 0; //valueは評価値
-    
-    //駒の価値　(初期値)について 
+
+    // board_type board;
+    // make_board(state, &board);
+
+    //駒の価値　(初期値)について
 
     //if文の中身がどう書けば良いのか迷走したので　
     //歩とと金と持ち歩の3箇所だけやっていただけるとそれを真似てやれるので教えていただきたいです
-    if (0) value += 110; //歩があったら評価値+ 110
+    value += 110 * count_pieces(state, PIECE_FU, is_first, false, false); // 歩があったら評価値+ 110
     if (0) value += 810; // 銀があったら
     if (0) value += 910; //金があったら
     if (0) value += 1290; //角があったら
     if (0) value += 1670; // 飛車があったら
-    if (0) value += 890; // と金があったら
+    value += 890 * count_pieces(state, PIECE_FU, is_first, false, true); // と金があったら
     if (0) value += 930; // 成り銀があったら
     if (0) value += 1980; // 馬があったら
     if (0) value += 2400; // 龍があったら
-    if (0) value += 150; // 持ち歩があったら
+    value += 150 * count_pieces(state, PIECE_FU, is_first, true, false); // 持ち歩があったら
     if (0) value += 1100; // 持ち銀があったら
     if (0) value += 1260; // 持ち金があったら
     if (0) value += 1460; // 持ち角があったら
@@ -63,6 +86,58 @@ int eval( /*引数教えていただきたいです*/  ){
     //どう表現したら良いのかわからないので
     //相手が後手だった時、相手玉が5E(11000)の時の歩の評価のところだけでもやっていただけると
     //あとはそれを真似してやれるので教えていただきたいです。
+    
+
+    // 歩
+    // 銀
+    // 金、と金、成り銀
+    // 角
+    // 飛車
+    // 馬
+    // 龍
+    // 自玉
+    uint8_t table[8][9][5] = {
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}
+    }; // 0～255
+
+    coord_type p = get_coord(state[PIECE_OU * 2 + (is_first ? 1 : 0)]);
+    int px = p % 5, py = p / 5;
+
+    for (int i = 0; i < 12; ++i) {
+        if (is_first_ones(state[i]) != is_first) {
+            continue;
+        }
+        coord_type c = get_coord(state[i]);
+        int cx = c % 5, cy = c / 5;
+        if (c == TEGOMA) {
+            continue;
+        }
+
+        if (cx > px) {
+            int tmp = cx;
+            cx = px;
+            px = tmp;
+        }
+
+        int kind;
+        if (i / 2 == PIECE_FU) {
+            kind = (is_promoted(state[i]) ? 2 : 0);
+        }
+        else if (i / 2 == PIECE_GI) {
+            kind = (is_promoted(state[i]) ? 2 : 1);
+        }
+        // ...以下同様
+
+
+        value += table[kind][py - cy + 4][px - cx];
+    }    
 
     //相手が後手だった時、相手玉が5E(11000)の時
     //歩
